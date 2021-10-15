e3c257a74986f483fafc2dea6229c395c84041b9
51sec <johnyan.ca@gmail.com>
2021-10-14 22:38:36 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '114',
+  ver: '115',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -42,7 +42,6 @@ jsproxy_config({
     // 该节点用于加载大体积的静态资源
     'cfworker': {
       label: 'cfworker',
-      hidden: false,
       lines: {
         // 收费版（高权重）
         'node-cfworker-2.etherdream.com': 4,
