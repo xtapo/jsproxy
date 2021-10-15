48dce160682f19dcf7389b90c812abf841d23d2a
51sec <johnyan.ca@gmail.com>
2021-10-14 22:49:27 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '116',
+  ver: '117',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -12,6 +12,12 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
+   'mysite': {
+      label: 'Local',
+      lines: {
+        [location.host]: 1,
+      }
+    },
     'demo-hk': {
       label: 'Demo - Hongkong',
       lines: {
@@ -24,19 +30,6 @@ jsproxy_config({
       label: 'Demo - Singapore',
       lines: {
         'node-aliyun-sg.etherdream.com:8443': 1,
-      }
-    },
-    'mysite': {
-      label: 'Local',
-      lines: {
-        [location.host]: 1,
-      }
-    },
-    'north-america': {
-      label: 'North America',
-      lines: {
-        // 主机:权重
-        'proxy.itprosec.eu.org': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
