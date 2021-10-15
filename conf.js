7ba93caab2d9685cba6703e79fb4c87342814347
51sec <johnyan.ca@gmail.com>
2021-10-14 22:10:51 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '112',
+  ver: '113',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -60,7 +60,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'cfworker',
+  node_default: 'mysite',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
